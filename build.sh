778a36e240d20df01d9b2d49dc28766004326131
Dan Ivy <danivy4@gmail.com>
2019-11-02 21:10:17 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+function main {
+  if [[ "$#" -ne 1 ]]; then
+    echo 'Usage: ./build.sh TEMPLATE_DIRECTORY_PATH'
+    return 1
+  fi
+  cd "$1"
+  packer validate -var-file=variables.json template.json
+  packer build -var-file=variables.json template.json
+}
+
+function echo_err {
+  >&2 echo "$@"
+}
+
+main "$@"
