ac7f2664d11acba43d7315b7b0f4e061a6e80203
Dan Ivy <danivy4@gmail.com>
2019-12-26 00:15:54 -0600

    Add powershell build script

---
+++ build.ps1
@@ -0,0 +1,40 @@
+param(
+  [Parameter(HelpMessage="What template to build.", Mandatory)]
+  [string]$TemplateDirectory
+)
+
+$ErrorActionPreference = "Stop"
+
+function Main {
+  $TemplateDirectory = $args[0]
+  $StartingDir = Get-Location
+  try {
+    Set-Location $TemplateDirectory
+    $PackerArgs = '-var',"vm_base_directory=$(Get-VMBaseDirectory)",'-var-file=variables.json','template.json'
+    packer validate @PackerArgs
+    packer build -force @PackerArgs
+  } finally {
+    Set-Location $StartingDir
+  }
+}
+
+function Get-VMBaseDirectory {
+  $dir = $env:PACKER_VM_DIR
+  if ($dir -eq $null) {
+    $dir = Get-VBoxVMDirectory
+  }
+  if (-Not (Test-Path $dir)) {
+    Write-Error "Invalid vm directory: '$dir' is not a directory"
+    return $false
+  }
+  return $dir
+}
+
+function Get-VBoxVMDirectory {
+  return $( `
+      VBoxManage list systemproperties `
+      | Select-String -Pattern '^Default machine folder:' `
+    ) -replace '^Default machine folder:\s*', ''
+}
+
+Main $TemplateDirectory
